0531d7b04c697f6dc66d22750ba57f74b40bb772
Egor Hohlachev <egor.khokhlachev@math.msu.ru>
2026-03-05 21:45:49 +0300

    7

--- 7.py
+++ 7.py
@@ -8,7 +8,19 @@ l = 1
 
 while i < Len:
     if arr[i] == arr[j]:
-        while arr[i] == arr[j]:
+        while (j < Len) and (arr[i] == arr[j]):
             j += 1
             l += 1
+        for k in range(i + l,len(arr)):
+            arr[k - l + 1] = arr[k]
+        i += 1
+        j = i + 1
+        Len = Len - (l - 1)
+        l = 1
+    else:
+        i += 1
+        j += 1
+
+for i in range(Len):
+    print(arr[i], end=" ")      
         
